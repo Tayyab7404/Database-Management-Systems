13d71148c80fb2628882f12a2ceb2e38c302685d
Tayyab <98115384+Tayyab7404@users.noreply.github.com>
2023-05-02 16:33:10 +0530

    Add files via upload

---
+++ WEEK 4 (2).sql
@@ -0,0 +1,63 @@
+/*
+WEEK 4:
+
+Write nested queries for answering the following queries that retrieve data from university database:
+
+1. Find the total number of (distinct) students who have taken course sections taught by the instructor with ID 110011
+*/
+
+/*
+2. Find the names of all instructors whose salary is greater than at least one instructor in the Biology department.
+*/
+
+/*
+3. Find the departments that have the highest average salary.
+*/
+
+/*
+4. Find all the courses taught in the both the Fall 2009 and Spring 2010 semesters.
+*/
+
+/*
+5. Find all the courses taught in the Fall 2009 semester but not in the Spring 2010 semester.
+*/
+
+/*
+6. Find all courses taught in both the Fall 2009 semester and in the Spring 2010 semester. (Write correlated nested Query)
+*/
+
+/*
+7. Find all students who have taken all courses offered in the Biology department. (Write Correlated nested Query)
+*/
+
+/*
+8. Find all courses that were offered at most once in 2009.
+*/
+
+/*
+9. Find all courses that were offered at least twice in 2009.
+*/
+
+/*
+10. Find the average instructors salaries of those departments where the average salary is greater than $42000.
+*/
+
+/*
+11. Find the departments with the maximum budget.
+*/
+
+/*
+12. Find the names of instructors who have not taught any course.
+*/
+
+/*
+13. Find the IDs and names of all students who have not taken any course offering before Spring 2009.
+*/
+
+/*
+14. Find the lowest, across all departments, of the per-department maximum salary computed.
+*/
+
+/*
+15. Display the IDs and names of the instructors who have taught all Comp. Sci. courses.
+*/
